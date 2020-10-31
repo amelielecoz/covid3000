f2ee6f3ef026e0ff9c94840d4921b3ead73e0fa7
Lx4 <franck.farcy@gmail.com>
2020-10-31 01:02:16 +0100

    adding history selectors, may move some of the code server side (number of days...)

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -2,9 +2,10 @@ import React from "react";
 
 import CounterCard from "../CounterCard";
 
-import { getHospitalsData } from "../../selectors/hospitals";
+import { getHospitalsData, getHistory } from "../../selectors/hospitals";
 
 const Dashboard = () => {
+  const history = getHistory("2020-10-28"); // hardcoded history (but selector code is valid, may move server side)
   const hospitalsData = getHospitalsData("2020-10-28"); // hardcoded day | to be chosen in a selector
   const hospitalsDataDayBefore = getHospitalsData("2020-10-27"); // hardcoded day | to be day - 1 from the previous one
   const delta = {
--- frontend/src/selectors/hospitals.js
+++ frontend/src/selectors/hospitals.js
@@ -1,3 +1,5 @@
+import { subDays } from "date-fns";
+
 import hospitals_data from "../data/hospitals_data.json";
 
 // return hospital data from a specific day
@@ -16,11 +18,42 @@ export const getHospitalsData = (day) => {
 };
 
 // return an object which contains the delta between specified day and the day before
+// this function may be included in getHospitalsData
 export const getDeltaDayBefore = (day) => {
   const data_day = getHospitalsData(day);
   //TODO
 };
 
-export const getHistory = (day, numberDays = 10) => {
-  // get the last 10 days of data
+// take a date with format like "2020-10-28" and return the last 10 days of data with 1 line per day
+export const getHistory = (day, daysToHistory = 10) => {
+  const dayDate = new Date(day);
+  const subDate = subDays(dayDate, daysToHistory);
+  const filtered_data = hospitals_data.filter(
+    ({ jour, sexe }) => sexe === 0 && new Date(jour) >= subDate
+  );
+  return filtered_data.reduce((historyList, currentValue) => {
+    if (!historyList.find((element) => element.jour === currentValue.jour))
+      return [
+        ...historyList,
+        {
+          jour: currentValue.jour,
+          hosp: currentValue.hosp,
+          rea: currentValue.rea,
+          dc: currentValue.dc,
+          rad: currentValue.rad,
+        },
+      ];
+    else
+      return historyList.map((element) => {
+        if (element.jour === currentValue.jour) {
+          return {
+            jour: element.jour,
+            hosp: element.hosp + currentValue.hosp,
+            rea: element.rea + currentValue.rea,
+            dc: element.dc + currentValue.dc,
+            rad: element.rad + currentValue.rad,
+          };
+        } else return element;
+      });
+  }, []);
 };
