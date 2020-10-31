c890a9da1222c90986d7b469b4bf25024a2fcc96
Lx4 <franck.farcy@gmail.com>
2020-10-31 01:16:43 +0100

    small fix

--- frontend/src/components/HistoryRow.js
+++ frontend/src/components/HistoryRow.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const HistoryRow = ({ data }) => {
   return (
-    <div className="flex flex-row items-center bg-gray-200 mt-2 h-24">
+    <div className="flex flex-row text-xs items-center bg-gray-200 mt-2 h-24">
       {data.jour}
+      <span className="ml-2">hosp: {data.hosp}</span>
+      <span className="ml-2">rea: {data.rea}</span>
+      <span className="ml-2">dc: {data.dc}</span>
+      <span className="ml-2">rad: {data.rad}</span>
     </div>
   );
 };
