7035da5f2dc950ceda443d367c31db0e50eec6cd
Lx4 <franck.farcy@gmail.com>
2020-11-10 17:44:01 +0100

    working on text element positioning into the groups and relative to rect

--- frontend/src/components/charts/BarChart.js
+++ frontend/src/components/charts/BarChart.js
@@ -5,28 +5,22 @@ import "./BarChart.css";
 
 const BarChart = ({ history }) => {
   /* D3 Practice*/
-  const DAY_HISTORY = 365;
+  const DAY_HISTORY = 10;
   const svgWidth = 343;
   const svgHeight = 170;
   const svgEl = useRef(null);
 
   // Set of data to work with
-  // TODO : we should keep the date here to have the right data 'reference' in d3, have to dig currently looking courses on FEM
-  const historyHosp = history.slice();
-
-  //   history
-  //     .slice(history.length - DAY_HISTORY, history.length)
-  //     .map((d) => ({ jour: d.jour, hosp: d.hosp }));
-
-  console.log("historyHop" + historyHosp.length);
-  console.log(historyHosp);
+  const historyHosp = history
+    .slice(history.length - DAY_HISTORY, history.length)
+    .map((d) => ({ jour: d.jour, hosp: d.hosp }));
 
   // Prepare displayable data
   const xScale = d3.scaleBand().domain(historyHosp.keys()).range([0, svgWidth]);
   //.padding(0.1); // fix padding for now
 
-  const min = 0;
   const max = d3.max(historyHosp, (d) => d.hosp);
+  const min = d3.min(historyHosp, (d) => d.hosp) - 40; //
 
   const yScale = d3.scaleLinear().domain([min, max]).range([svgHeight, 0]);
 
@@ -43,23 +37,28 @@ const BarChart = ({ history }) => {
     .attr("width", xScale.bandwidth())
     .attr("x", (d, i) => xScale(i))
     .attr("stroke-width", 3)
-    .attr("stroke", "pink")
-    .attr("fill", "pink");
+    .attr("stroke", "blue")
+    .attr("fill", "blue")
+    .attr("fill-opacity", ".6");
   g.select(".nbHosp").text((d) => d.hosp);
   g.select(".jour").text((d) => d.jour);
 
   // entering, just once the first time it loads, then only requested (height and y) will be updated
-  const target = g.enter().append("g");
+  const target = g
+    .enter()
+    .append("g")
+    .attr("x", (d, i) => xScale(i));
   target
     .append("rect")
-    .attr("x", (d, i) => xScale(i))
     .attr("y", (d) => yScale(d.hosp))
     .attr("width", xScale.bandwidth())
     .attr("height", (d) => svgHeight - yScale(d.hosp));
   target
     .append("text")
     .classed("nbHosp", true)
-    .text((d) => d.hosp);
+    .text((d) => d.hosp)
+    .attr("x", (d, i) => xScale(i) + xScale.bandwidth() / 2)
+    .attr("y", svgHeight);
   target
     .append("text")
     .classed("jour", true)
@@ -68,40 +67,6 @@ const BarChart = ({ history }) => {
   // removing
   console.log(g.exit());
   g.exit().remove(); //data that exits the initial array
-
-  // .data(historyHosp, (d) => d)
-  // .join(
-  //   (enter) => {
-  //     const g = enter.append("g");
-  //     g.append("rect").attr("y", svgHeight).attr("height", 0);
-  //     g.append("text");
-  //     return g;
-  //   },
-  //   (update) => update,
-  //   (exit) => {
-  //     exit
-  //       .select("rect")
-  //       .transition(t)
-  //       // everything after here is transition TO
-  //       .attr("y", svgHeight)
-  //       .attr("height", 0);
-  //   }
-  // )
-  // .select("rect")
-  // .attr("x", (d, i) => xScale(i))
-  // .attr("y", (d) => yScale(d.hosp))
-  // .attr("width", xScale.bandwidth())
-  // .transition(t)
-  // .attr("height", (d) => svgHeight - yScale(d.hosp));
-
-  // .attr("stroke-width", 3)
-  // .attr(
-  //   "stroke-dasharray",
-  //   `${Math.floor(Math.random() * 6) + 1} ${
-  //     Math.floor(Math.random() * 6) + 1
-  //   }`
-  // )
-  // .attr("stroke", "plum");
 
   return (
     <svg className="" ref={svgEl} height={svgHeight} width={svgWidth}></svg>
