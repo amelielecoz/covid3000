ccb865a0040ad91bc4416dc2b8f11833dce5860f
Lx4 <franck.farcy@gmail.com>
2020-11-17 18:06:27 +0100

    fixing timers

--- frontend/src/components/CounterCard.js
+++ frontend/src/components/CounterCard.js
@@ -34,7 +34,7 @@ const Counter = ({ number, delta, text, type }) => {
       }}
       className={`w-24 pb-1  bg-white rounded shadow border-b-4 ${getColor(
         "border"
-      )} text-center transition duration-500 ease-in-out hover:shadow-xl hover:bg-blue-100 transform hover:-translate-y-1 hover:scale-110 `}
+      )} text-center transition duration-100 ease-in-out hover:shadow-xl hover:bg-blue-100 transform hover:-translate-y-1 hover:scale-110 `}
     >
       <div className="h-2 text-xs text-gray-700 font-semibold">{text}</div>
       <div className="my-1">
